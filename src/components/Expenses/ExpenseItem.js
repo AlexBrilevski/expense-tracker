50b01530c8e39a6e320714fcb12135eb6323d2b1
Alex Brilevski <brilevski.alex@gmail.com>
2026-03-28 14:17:47 +0300

    Redundant state management in the expense item component was removed

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,16 +1,14 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
-const ExpenseItem = ({ date, title: propsTitle, amount }) => {
-  const [title, setTitle] = useState(propsTitle);
+const ExpenseItem = ({ date, title, amount }) => {
   return (
     <Card className="expense-item">
       <ExpenseDate date={date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
-        <button onClick={() => setTitle('Updated')}>Change title</button>
         <div className="expense-item__price">
           ${amount}
         </div>
