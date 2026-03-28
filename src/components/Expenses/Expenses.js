0202e989292e300e2c1cfd974b672696f2d529fb
Alex Brilevski <brilevski.alex@gmail.com>
2026-03-28 11:30:11 +0300

    The Expenses filter component was added

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
 import ExpenseItem from "./ExpenseItem";
 import './Expenses.css';
 
 const Expenses = ({items}) => {
   return (
     <Card className="expenses">
+      <ExpensesFilter />
       {items.map(item => (
         <ExpenseItem key={item.id} {...item} />
       ))}
