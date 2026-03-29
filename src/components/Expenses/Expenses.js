d1c48cf0859a716806de3d0c3c53091f4a590a93
Alex Brilevski <brilevski.alex@gmail.com>
2026-03-29 09:37:24 +0300

    Logic of filtering the expenses list by year was added

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -4,8 +4,12 @@ import ExpensesFilter from './ExpensesFilter';
 import ExpenseItem from "./ExpenseItem";
 import './Expenses.css';
 
-const Expenses = ({items}) => {
+const Expenses = ({ items }) => {
   const [selectedYear, setSelectedYear] = useState('2020');
+
+  const filteredItems = items.filter(item => {
+    return item.date.getFullYear().toString() === selectedYear;
+  });
 
   const onSelectYear = (year) => {
     setSelectedYear(year);
@@ -13,8 +17,8 @@ const Expenses = ({items}) => {
 
   return (
     <Card className="expenses">
-      <ExpensesFilter selected={selectedYear} onFilter={onSelectYear}/>
-      {items.map(item => (
+      <ExpensesFilter selected={selectedYear} onFilter={onSelectYear} />
+      {filteredItems.map(item => (
         <ExpenseItem key={item.id} {...item} />
       ))}
     </Card>
